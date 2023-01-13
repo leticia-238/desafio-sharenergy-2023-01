ed35c77594a4c95002f04ed1200cb6509d61b708
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2023-01-13 17:27:53 -0300

    🦄 refactor: altera configuração das rotas do app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
-import Private from './layouts/Private';
-import Login from './pages/Login';
-import HttpStatusCat from './pages/HttpStatusCat';
-import UserList from './pages/UserList';
-import CustomerManagement from './layouts/CustomerManagement';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Private /> }>
-        <Route index element={ <UserList /> } />
-        <Route path="http-status-cat" element={ <HttpStatusCat /> } />
-        <Route path="customers" element={ <CustomerManagement /> }>
-          <Route index element={ <h2>Customer List</h2> } />
-          <Route path="edit" element={ <h2>Customer Edit</h2> } />
-        </Route>
-      </Route>
-      <Route path="/login" element={ <Login /> } />
-    </Routes>
+    <>
+      <h1>Private Route</h1>
+      <Outlet />
+    </>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,38 @@
+/* eslint-disable react/jsx-key */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 import './index.css';
+import Private from './layouts/Private';
+import Login from './pages/Login';
+import HttpStatusCat from './pages/HttpStatusCat';
+import UserList from './pages/UserList';
+import CustomerManagement from './layouts/CustomerManagement';
+import AuthProvider from './contexts/AuthContext';
+
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <Route path="/" element={ <Private /> }>
+      <Route index element={ <UserList /> } />
+      <Route path="http-status-cat" element={ <HttpStatusCat /> } />
+      <Route path="customers" element={ <CustomerManagement /> }>
+        <Route index element={ <h2>Customer List</h2> } />
+        <Route path="edit" element={ <h2>Customer Edit</h2> } />
+      </Route>
+    </Route>,
+    <Route path="/login" element={ <Login /> } />,
+  ]),
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <RouterProvider router={ router } />
+    </AuthProvider>
   </React.StrictMode>,
 );
