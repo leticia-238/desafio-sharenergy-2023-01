14592fa1d28233587541482e981efee50da54df5
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2023-01-19 20:19:10 -0300

    ✨ feat: adiciona cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import AuthController from './controllers/AuthController';
 import UserModel from './models/UserModel';
 
@@ -6,9 +7,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors({ origin: 'http://localhost:5173' }));
+
 const userModel = new UserModel();
 const authController = new AuthController(userModel);
 
-app.post('/auth', authController.login);
+app.post('/auth', cors(), authController.login);
 
 export default app;
