ab2702e54b6dc5add7e320a736d87df131d7a74f
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2023-01-20 16:13:15 -0300

    ✨ feat: estilização do login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
 import authApi from '../services/authApi';
@@ -16,8 +16,12 @@ function Login() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const token = await authApi({ username, password });
+      const token = await authApi({ user: { username, password } });
       setToken(token);
+      if (rememberCheck) {
+        const parsedData = JSON.stringify({ username, password });
+        localStorage.setItem(parsedData);
+      }
       navigate('/');
     } catch (error) {
       setInvalidLogin(true);
@@ -25,55 +29,69 @@ function Login() {
     }
   };
 
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      setUsername(user.username);
+      setPassword(user.password);
+    }
+  }, []);
+
   return (
-    <form onSubmit={ async (e) => { await handleSubmit(e); } }>
-      {invalidLogin && (
-        <p>Username ou senha incorretos. Tente novamente!</p>
-      )}
-      <label
-        className="rounded-lg bg-gray-100 border-2
+    <div className="min-h-screen bg-gradient-to-r from-cyan-400 to-green-400 p-4">
+      <form
+        className="grid gap-4 bg-white p-2 rounded-md mt-44"
+        onSubmit={ async (e) => { await handleSubmit(e); } }
+      >
+        {invalidLogin && (
+          <p>Username ou senha incorretos. Tente novamente!</p>
+        )}
+        <legend className="font-bold text-center text-2xl text-cyan-600">Login</legend>
+        <label
+          className="rounded-lg bg-gray-100 border-2
         border-gray-200 flex items-center"
-        htmlFor="username"
-      >
-        <input
-          type="text"
-          id="username"
-          placeholder="Username"
-          className="px-2 py-1.5  grow bg-inherit focus:outline-none"
-          value={ username }
-          onChange={ ({ target }) => setUsername(target.value) }
-        />
-      </label>
-      <label
-        className="rounded-lg bg-gray-100 border-2
+          htmlFor="username"
+        >
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            className="px-2 py-1.5  grow bg-inherit focus:outline-none"
+            value={ username }
+            onChange={ ({ target }) => setUsername(target.value) }
+          />
+        </label>
+        <label
+          className="rounded-lg bg-gray-100 border-2
         border-gray-200 flex items-center"
-        htmlFor="password"
-      >
-        <input
-          type="password"
-          id="password"
-          placeholder="Password"
-          className="px-2 py-1.5  grow bg-inherit focus:outline-none"
-          value={ password }
-          onChange={ ({ target }) => setPassword(target.value) }
-        />
-      </label>
-      <label htmlFor="remember-me">
-        Remember me
-        <input
-          type="checkbox"
-          id="remember-me"
-          checked={ rememberCheck }
-          onChange={ () => setRemembercheck(!rememberCheck) }
-        />
-      </label>
-      <button
-        type="submit"
-        className="rounded-lg px-4 py-2 text-white font-semibold bg-cyan-500"
-      >
-        Sign in
-      </button>
-    </form>
+          htmlFor="password"
+        >
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            className="px-2 py-1.5  grow bg-inherit focus:outline-none"
+            value={ password }
+            onChange={ ({ target }) => setPassword(target.value) }
+          />
+        </label>
+        <label htmlFor="remember-me" className="justify-self-center p-2">
+          <input
+            type="checkbox"
+            id="remember-me"
+            checked={ rememberCheck }
+            onChange={ () => setRemembercheck(!rememberCheck) }
+          />
+          {' Remember me'}
+        </label>
+        <button
+          type="submit"
+          className="rounded-lg px-4 py-2 text-white font-semibold bg-cyan-500"
+        >
+          Sign in
+        </button>
+      </form>
+    </div>
   );
 }
 
